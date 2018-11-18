615c42743c7a92f20144fee5b620c93e9bab078c
Pluscrafter <pluscrafter123@hotmail.com>
2018-11-18 02:23:49 +0100

    add DATALST.save

--- src/UT61E.h
+++ src/UT61E.h
@@ -3,6 +3,9 @@
 #include <sstream>
 #include <vector>
 
+#define DC false
+#define AC true
+
 class UT61E{
 public:
 	UT61E();
@@ -11,6 +14,50 @@ public:
 	std::string getSigrokOut();
 	std::vector<std::string> getSplitOut();
 private:
-	
+	std::string page0[14] = {
+		"mV",
+		"V",
+		"Hz",
+		"%",
+		"Ω",
+		"kΩ",
+		"MΩ",
+		"pF",
+		"nF",
+		"µF",
+		"mF",
+		"µA",
+		"mA",
+		"A"
+	};
+
+	std::string page1[6] = {
+		"DC",
+		"AC",
+		"AUTO",
+		"HOLD",
+		"REL",
+		"DIODE"
+	};
+
+	std::string page2[3]{
+		"AUTO",
+		"REL",
+		"HOLD"
+	};
+
+	std::string page3[1]{
+		"REL"
+	};
+
+	std::string page4[1]{
+		"REL"
+	};
+
+	bool Automode 	= false;
+	bool Hold	= false;
+	bool Relative	= false;
+	bool DAmode	= false;
+
 };
 
