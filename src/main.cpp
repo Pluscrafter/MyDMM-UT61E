59f57d8187b041b1abaaeba5b2250ae8c5c1387a
Pluscrafter <pluscrafter123@hotmail.com>
2018-12-05 15:06:28 +0100

    add Temperature measurment

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char **argv){
 	UT61E ut;
 	std::ofstream data;
 
-	
+	float etime = 0;
 
 	while(true){
 		auto start = std::chrono::steady_clock::now();
@@ -18,9 +18,10 @@ int main(int argc, char **argv){
 		std::cout << ut.readTemp() << std::endl;
 		auto stop = std::chrono::steady_clock::now();
 		float time = std::chrono::duration_cast<std::chrono::milliseconds>(stop-start).count();
-		std::cout << time/1000 <<std::endl;
+		std::cout << etime/1000 <<std::endl;
+		etime += time;
 		data.open(argv[1], std::ofstream::app);
-		data << time/1000 << "	" << ut.readTemp() << std::endl;
+		data << etime/1000 << ";" << ut.readTemp() << std::endl;
 		data.close();
 		
 
